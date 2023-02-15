05f1ca12b6bdd677da3595f298fd253d00c69b93
white160114 <2220128@ecc.ac.jp>
2023-02-15 15:48:02 +0900

    バックエンド制作開始

---
+++ addresult.php
@@ -0,0 +1,21 @@
+<?php
+    // リクエスト形式がPOSTでなければhome.htmlに遷移
+    if($_SERVER["REQUEST_METHOD"] !== "POST"){
+        header("Location: home.html");
+        exit;
+    }
+
+    // フォームデータ取得
+    $site_name = filter_input(INPUT_POST, "site_name");
+    $url = filter_input(INPUT_POST, "url");
+
+    //結果保存用配列
+    $result = [
+        "status"  => true, //エラーがあった場合false
+        "message" => null, //表示するメッセージ
+        "result"  => null, //更新結果（件数）
+    ];
+
+    
+
+?>
