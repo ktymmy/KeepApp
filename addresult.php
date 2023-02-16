ec7b20b3531daf5272f3171da9cedde0538970f7
white160114 <2220128@ecc.ac.jp>
2023-02-16 15:59:27 +0900

    adddatabaseのphp部分完成

--- addresult.php
+++ addresult.php
@@ -19,11 +19,66 @@
         "result"  => null, //更新結果（件数）
     ];
     try{
-       
-    }catch{
+       $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
+       $db = new PDO($dsn, "kp_user", "ecc");
+       //接続の属性設定 ATTR_EMULATE_PREPARESはいつもfalseにしておく
+       $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       $db->beginTransaction();
 
+       // スペースを空文字に置き換える
+       $site_name = str_replace(array(" ","　"),"",$site_name);
+       if(!$site_name){
+        $result['status'] = false;
+        $result['massage'] = "名前を入力してください。";
+       }
+
+       //URL空文字チェック
+       if(!$url){
+        $result['status'] = false;
+        $result['massage'] = "URLを入力してください。";
+       }
+
+       $sql_cnt = "SELECT COUNT(site_name) FROM keep WHERE site_name = :site_name";
+       $stmt = $db->prepare($sql_cnt);
+       $stmt->bindParam('product_no', $product_no);
+       $stmt->execute();
+       if ($stmt->fetch()[0] == 1) {
+        $result['status'] = false;
+        $result['message'] = "既に登録されています";
+       }
+
+       // いったん切断
+       $stmt = null;
+       $db = null;
+       //DB接続
+       $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
+       $db = new PDO($dsn, "kp_user", "ecc");
+       //接続の属性設定 ATTR_EMULATE_PREPARESはいつもfalseにしておく
+       $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+       if($result['status']){
+        $sql = "INSERT INTO keep VALUES(:site_name, :url)";
+        //SQLの準備
+        $stmt = $db->prepare($sql);
+        //データのバインド
+        $stmt->bindParam('site_name', $site_name, PDO::PARAM_STR);
+        $stmt->bindParam('url', $url, PDO::PARAM_STR);
+        // SQL実行(戻り値は変更した件数)
+        $result["result"] = $stmt->execute();
+        //結果が1(1件挿入できた)ときはコミットする
+        if ($result["result"] !== 0) {
+         $db->commit();
+         $result["message"] = "データ登録に成功しました！";
+        }
+    }
+    }catch(PDOException $poe){
+        $db->rollBack(); // ⑥ロールバック
+        echo "DB接続エラー" . $poe->getMessage();
     }finally{
-        
+        $stmt = null;
+        $db = null;
     }
 
 
