c436bf581ac39a0fab0007dc195e0288527d65b2
white160114 <2220128@ecc.ac.jp>
2023-02-17 16:39:16 +0900

    list制作開始

--- list.php
+++ list.php
@@ -1,3 +1,42 @@
+<?php
+    require_once __DIR__ . "/def.php";
+    require_once __DIR__ . "/utils.php";
+    $site_name = filter_input(INPUT_GET, "site_name");
+    $url = filter_input(INPUT_GET, "url");
+
+    $dsn = "mysql:host=localhost;dbname=studb;charset=utf8mb4";
+    try{
+        $db = new PDO($dsn, "dbuser", "ecc");
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $sql = "SELECT * FROM keep_url ";
+        $where = "";
+        $nameLike = "";
+        if($site_name != ""){
+            $nameLike = "%".$site_name."%";
+            $where = " WHERE site_name like :site_name ";
+        }
+
+        $stmt = $db->prepare($sql.$where);
+        if($site_name != ""){
+            $stmt->bindParam(":site_name",$nameLike,PDO::PARAM_STR);
+        }
+
+        $stmt->execute();
+
+        $result = [];
+        while($rows = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $result[] = $rows;
+        }
+        $stmt = null;
+        $db = null;
+    }catch(PDOException $poe){
+        exit("DBエラー".$poe->getMessage());
+    }
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -31,7 +70,7 @@
                     <tr>
                         <td> 
                             <div class="t_button">
-                                <a href="https://cpoint-lab.co.jp/article/202003/14512/"><span>aaa</span></a>
+                                <a href="https://cpoint-lab.co.jp/article/202003/14512/"><span></span></a>
                             </div>
                         </td>
                     </tr>
