ab4caea117150d5d9edfc7778502bad8c256e557
white160114 <2220128@ecc.ac.jp>
2023-02-22 16:34:50 +0900

    update

--- list.php
+++ list.php
@@ -3,6 +3,8 @@
     require_once __DIR__ . "/utils.php";
     $site_name = filter_input(INPUT_GET, "site_name");
     $url = filter_input(INPUT_GET, "url");
+    $category = filter_input(INPUT_GET, "category");
+
 
     $dsn = "mysql:host=localhost;dbname=keep;charset=utf8mb4";
     try{
@@ -11,15 +13,36 @@
         $sql = "SELECT * FROM keep_url ";
         $where = "";
         $nameLike = "";
+        $categorywhere = "";
         if($site_name != ""){
             $nameLike = "%".$site_name."%";
             $where = " WHERE site_name like :site_name ";
         }
+        if($category != 1 && $category != ""){
+            if($category == 2){
+              $categorywhere = "favorite";
+            }else{
+              $categorywhere = "list";
+            }
+            if($name != ""){
+              $where = $where."AND category = :category";
+            }else{
+              $where = "WHERE category = :category";
+            }
+          }
 
         $stmt = $db->prepare($sql.$where);
         if($site_name != ""){
             $stmt->bindParam(":site_name",$nameLike,PDO::PARAM_STR);
         }
+
+        if($url != ""){
+            $stmt->bindParam(":url",$categorywhere,PDO::PARAM_STR);
+          }
+
+        if($category != ""){
+            $stmt->bindParam(":category",$categorywhere,PDO::PARAM_STR);
+          }
 
         $stmt->execute();
 
